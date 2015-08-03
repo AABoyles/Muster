8fe116f09c2a07221c358c4eb268a57cebcd2763
Tony Boyles <aaboyles@gmail.com>
2015-08-03 20:45:31 +0000

    Added a neat D3.js graph whenever you have Estimated a submission.

--- api/estimate.php
+++ api/estimate.php
@@ -3,22 +3,20 @@
 require_once("creds.php");
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    $result = mysqli_query($db, "INSERT INTO  `vote` (`submissionid`, `isreal`) VALUES ('".
-        mysqli_real_escape_string($db, $_POST['id'])."',".
-        mysqli_real_escape_string($db, $_POST['real']).")");
+    $result = mysqli_query($db, "INSERT INTO  `estimate` (`submissionid`, `estimate`) VALUES (".
+        mysqli_real_escape_string($db, $_POST['sid']).",".
+        mysqli_real_escape_string($db, $_POST['estimate']).")");
 }
 
-$result1 = mysqli_query($db, "SELECT count(1) as realVotes FROM `vote` WHERE submissionid = ".mysqli_real_escape_string($db, $_REQUEST['id'])." AND isreal = 1");
-$votesreal = mysqli_fetch_assoc($result1)['realVotes'];
+$result2 = mysqli_query($db, "SELECT estimate as estimateValue, count(1) as numberOfEstimates FROM `estimate` WHERE submissionid = ".
+    mysqli_real_escape_string($db, $_REQUEST['sid'])." GROUP BY estimate;");
+$estimates = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
-$result2 = mysqli_query($db, "SELECT count(1) as fakeVotes FROM `vote` WHERE submissionid = ".mysqli_real_escape_string($db, $_REQUEST['id'])." AND isreal = 0");
-$votesfake = mysqli_fetch_assoc($result2)['fakeVotes'];
-
-$result3 = mysqli_query($db, "SELECT realop FROM `submission` WHERE sid = ".mysqli_real_escape_string($db, $_REQUEST['id']));
+$result3 = mysqli_query($db, "SELECT realop FROM `submission` WHERE sid = ".
+    mysqli_real_escape_string($db, $_REQUEST['sid']));
 $isreal = mysqli_fetch_assoc($result3)['realop'];
 
 exit(json_encode(array(
-    "votesReal"=>$votesreal,
-    "votesFake"=>$votesfake,
+    "estimates"=>$estimates,
     "isReal"=>$isreal
 )));
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 app = {
   populateBody: function(){},
-  addUser: function(){}
+  addUser: function(){},
+  buildGraph: function(data){}
 };
 
 $(function(){
@@ -9,56 +10,56 @@ $(function(){
         $.getJSON("api/submission.php", {
             "cid": $("#category").val()
         }, function(ret){
-            var main = $("#main").fadeOut(400, function(){
+            var main = $("#main").slideUp(400, function(){
                 main.empty();
                 var lastName = "";
                 _(ret).each(function(el){
                     if(lastName != el.name){
-                        main.append("<h2 style='text-align:center;'>"+el.name+'</h2><div style="width:100%;text-align:center;margin-bottom:15px;"><button data-toggle="modal" data-target="#submissions" class="btn btn-default addAPosition" role="button">Submit Your Position</button></div><hr />');
+                        main.append("<h2 style='text-align:center;'>"+el.name+'</h2>'+
+                          "<div style='width:100%;text-align:center;margin-bottom:15px;'>"+
+                          "  <button data-toggle='modal' data-target='#submissions' class='btn btn-default addAPosition' role='button'>Submit Your Position</button>"+
+                          "</div><hr />");
                     }
-                    main.append("<blockquote>"+el.content+'</blockquote><div style="text-align:center;">'+
+                    main.append("<blockquote>"+el.content+"</blockquote>");
+                    if(!Cookies.get("votedFor"+el.sid)){
+                      main.append('<div style="text-align:center;">'+
                         '<p>How confident are you (in percentages) that the author <b>actually</b> holds this opinion?</p>'+
-                        '<button id="vote'+el.sid+'isreal" class="vote btn btn-default">1</button> ' +
-                        '<button id="vote'+el.sid+'isreal" class="vote btn btn-default">10</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">20</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">30</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">40</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">50</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">60</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">70</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">80</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">90</button> '+
-                        '<button id="vote'+el.sid+'isfake" class="vote btn btn-default">99</button>' +
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">1</button> ' +
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">10</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">20</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">30</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">40</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">50</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">60</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">70</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">80</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">90</button> '+
+                        '<button data-sid="'+el.sid+'" class="estimate btn btn-default">99</button>' +
                         '</div><hr />');
+                      $(".estimate").click(function(){
+                        var $this = $(this);
+                        $.post("api/estimate.php", {sid: $this.data("sid"), estimate: parseInt($this.text())}, function(ret){
+                            $this.parent().slideUp(400, function(){
+                                $(this).html("<p>How confident are you (in percentages) that the author <b>actually</b> holds this opinion?</p>")
+                                  .append("<svg id='graph"+$this.data("sid")+"'></svg>")
+                                  .slideDown();
+                                  app.buildGraph($this.data('sid'), ret);
+                            });
+                            Cookies.set("votedFor"+$this.data("sid"), true);
+                        }, "json");
+                      });
+                    } else {
+                      $.getJSON("api/estimate.php", {sid: el.sid}, function(ret){
+                        $('<div style="text-align:center;"><p>How confident are you (in percentages) that the author <b>actually</b> holds this opinion?</p></div>')
+                          .append('<svg id="graph'+el.sid+'"></svg>')
+                          .append("<hr />")
+                          .appendTo(main);
+                          app.buildGraph(el.sid, ret);
+                      });
+                    }
                 });
-                main.fadeIn(400);
+                main.slideDown(400);
             });
-        });
-        $(".vote").click(function(){
-            var $this = $(this);
-            if(Cookies.get("votedFor"+this.id)){
-                $.getJSON("api/estimate.php", {id: this.id, real: $this.hasClass("voteReal")}, function(ret){
-                    var newhtml =
-                        "Votes for Real: " + ret["votesReal"] + "<br />" +
-                        "Votes for Fake: " + ret["votesFake"] + "<br />" +
-                        "Was it Real? " + (ret["isReal"]=="1" ? "YUP." : "NOPE.");
-                    $this.parent().fadeOut(400, function(){
-                        $(this).html(newhtml).fadeIn();
-                    });
-                });
-            } else {
-                $.post("api/estimate.php", {id: this.id, real: $this.hasClass("voteReal")}, function(ret){
-                    var newhtml =
-                        "Votes for Real: " + ret["votesReal"] + "<br />" +
-                        "Votes for Fake: " + ret["votesFake"] + "<br />" +
-                        "Was it Real? " + (ret["isReal"]=="1" ? "YUP." : "NOPE.");
-                    $this.parent().fadeOut(400, function(){
-                        $(this).html(newhtml).fadeIn();
-                    });
-                    Cookies.set("votedFor"+this.id, true);
-                    Cookies.set("vote"+this.id, $this.hasClass("voteReal"));
-                }, "json");
-            }
         });
     };
 
@@ -80,6 +81,32 @@ $(function(){
         }, "json");
     };
 
+    app.buildGraph = function(sid, ret){
+      var parent = $("svg#graph"+sid).parent(),
+          margin = {top: 10, right: 20, bottom: 20, left: 30},
+          width = parent.width() - margin.left - margin.right,
+          height = 100 - margin.top - margin.bottom,
+          x = d3.scale.ordinal().rangeRoundBands([0, width], .1),
+          y = d3.scale.linear().range([height, 0]),
+          xAxis = d3.svg.axis().scale(x).orient("bottom"),
+          yAxis = d3.svg.axis().scale(y).orient("left").ticks(6, "d"),
+          svg = d3.select("svg#graph"+sid)
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      x.domain(["1","10","20","30","40","50","60","70","80","90","99"]);
+      y.domain([0, d3.max(ret.estimates, function(d) { return parseInt(d.numberOfEstimates); })]);
+      svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+      svg.append("g").attr("class", "y axis").call(yAxis);
+      svg.selectAll(".bar").data(ret.estimates).enter()
+          .append("rect").attr("class", "bar")
+            .attr("x", function(d) { return x(d.estimateValue); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(parseInt(d.numberOfEstimates)); })
+            .attr("height", function(d) { return height - y(parseInt(d.numberOfEstimates)); });
+    };
+
     $("#category").change(app.populateBody);
 
     $("#addATopic").click(function(){
@@ -96,17 +123,17 @@ $(function(){
 
     $("#submitTopic").click(function(){
         if(!Cookies.get("email")){
-            addUser($("#topicemail").val());
+            app.addUser($("#topicemail").val());
         }
         $.post("api/topic.php", {
             name:$("#topicname").val(),
             description:$("#topicdescription").is(":checked")?1:0
-        }, populateBody, "json");
+        }, app.populateBody, "json");
     });
 
     $("#submitPosition").click(function(){
         if(!Cookies.get("email")){
-            addUser($("#email").val());
+            app.addUser($("#email").val());
         }
         $.post("api/submission.php", {
             essay:$("#essay").val(),
