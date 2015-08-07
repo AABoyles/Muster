fc10e57436394097ba3e2925104731f6611be6bd
Tony Boyles <aaboyles@gmail.com>
2015-08-07 19:26:44 +0000

    Added a long-past-due update to the setup.sql file

--- setup.sql
+++ setup.sql
@@ -1,41 +1,77 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Aug 01, 2015 at 11:59 AM
--- Server version: 5.5.43-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `muster`
+--
+CREATE DATABASE IF NOT EXISTS `muster` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `muster`;
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `submission`
+-- Table structure for table `estimates`
 --
 
-CREATE TABLE IF NOT EXISTS `submission` (
-  `email` text NOT NULL,
+CREATE TABLE IF NOT EXISTS `estimates` (
+  `submissionid` int(11) NOT NULL,
+  `eid` int(11) NOT NULL AUTO_INCREMENT,
+  `estimate` tinyint(2) NOT NULL DEFAULT '50',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`eid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=65 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submissions`
+--
+
+CREATE TABLE IF NOT EXISTS `submissions` (
+  `userid` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `content` longtext NOT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `realop` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `topicid` int(11) NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vote`
+-- Table structure for table `topics`
 --
 
-CREATE TABLE IF NOT EXISTS `vote` (
-  `submissionid` int(11) NOT NULL,
-  `voteid` int(11) NOT NULL AUTO_INCREMENT,
-  `isreal` tinyint(1) NOT NULL DEFAULT '0',
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`voteid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=37 ;
+CREATE TABLE IF NOT EXISTS `topics` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `createdby` int(11) NOT NULL,
+  `createdat` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` longtext NOT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `email` text NOT NULL,
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `firstseen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `password` char(128) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
